0d312fd788db525d3ad95209ea0a45bb4ce55e50
Donnell Jackson <Donnell.Jackson@outlook.com>
2022-07-09 17:19:13 -0500

    adding updated code

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,26 @@
 
 class getData(inputURL:String) {      //input will be url or "q" to quit
 
+  val r = requests.get(inputURL)
+
   //method for api get request
   def executeRequest()={
 
-    val r = requests.get(inputURL)
-    if (r.statusCode == 200)
+    if (r.statusCode == 200) {
       println("Request Successful")
-    else if (inputURL == "q")
-      System.out.println("Exiting Program")
-    else
+    } else if (inputURL == "q") {
+      println("Exiting Application")
+      System.exit(0)
+    } else {
       println("Request Failed: Please try again")
+    }
+  }
+
+  def out(): Unit ={
+    var data = ujson.read(r.text)
+    println(r.headers)
+    println(" ")
+    println(data(0))
   }
 
 }
@@ -23,8 +33,10 @@ object Main{
 
       println("Input URL for request")
       var obj = new getData(scala.io.StdIn.readLine()) // read in user input as string
+      println(" ")
       obj.executeRequest()
-
+      println(" ")
+      obj.out()
     }
   }
 }
