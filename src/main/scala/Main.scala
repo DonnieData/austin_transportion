8f184b6796efa430c61143294e44c7a0745ce30a
Donnell Jackson <Donnell.Jackson@outlook.com>
2022-07-09 17:44:28 -0500

    working out recursion/application exiting bug

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,16 @@
 
-class getData(inputURL:String) {      //input will be url or "q" to quit
+class getInput(userInput:String) {      //input will be url or "q" to quit
 
-  val r = requests.get(inputURL)
+
+  if (userInput == "q") System.out.println("Exiting Application")
+  val r = requests.get(userInput)
 
   //method for api get request
   def executeRequest()={
-
-    if (r.statusCode == 200) {
+    if (r.statusCode == 200)
       println("Request Successful")
-    } else if (inputURL == "q") {
-      println("Exiting Application")
-      System.exit(0)
-    } else {
+      else
       println("Request Failed: Please try again")
-    }
   }
 
   def out(): Unit ={
@@ -32,7 +29,7 @@ object Main{
     while ( appControl == "run") {
 
       println("Input URL for request")
-      var obj = new getData(scala.io.StdIn.readLine()) // read in user input as string
+      var obj = new getInput(scala.io.StdIn.readLine()) // read in user input as string
       println(" ")
       obj.executeRequest()
       println(" ")
